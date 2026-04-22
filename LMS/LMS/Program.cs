d0abd2ead2c60ff1980f1c10df47a884f42fb387
vinh3690 <20222712@eaut.edu.vn>
2026-04-22 13:55:11 +0700

    Final sync: Point all API calls back to lms-u2jn.onrender.com

--- LMS/LMS/Program.cs
+++ LMS/LMS/Program.cs
@@ -96,12 +96,10 @@ builder.Services.AddAuthentication(options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
+        ValidateIssuer = false, // Tắt đi cho đỡ lỗi lệch domain
+        ValidateAudience = false, // Tắt đi cho đỡ lỗi lệch domain
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
     };
 
