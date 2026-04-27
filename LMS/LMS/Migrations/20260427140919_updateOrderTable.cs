617f32d19ac69fe9c97016b07f897cf8d25996e5
vinh3690 <20222712@eaut.edu.vn>
2026-04-27 21:10:21 +0700

    Add teacherId to OrderModel and update database

---
+++ LMS/LMS/Migrations/20260427140919_updateOrderTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "teacherId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "teacherId",
+                table: "Orders");
+        }
+    }
+}
